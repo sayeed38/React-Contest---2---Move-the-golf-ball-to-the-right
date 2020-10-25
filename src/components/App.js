73ee9760a8b66227c38b75464574da9baddfde55
sayeed38 <mohammedsayeed1997@gmail.com>
2020-10-25 18:07:52 +0530

    modified changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,50 @@
-import React, { Component, useState } from "react";
-import '../styles/App.css';
+import React, { Component } from "react";
+import "./styles.css";
 
 class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            renderBall: false,//variable needed to be changed
-            posi : 0,
-            ballPosition: { left: "0px" }
-        };
-        this.renderChoice = this.renderChoice.bind(this)
-        this.buttonClickHandler = this.buttonClickHandler.bind(this)
+  constructor(props) {
+    super(props);
+    this.state = {
+      renderBall: false, //variable needed to be changed
+      posi: 0,
+      ballPosition: { left: "0px" }
     };
+    this.renderChoice = this.renderChoice.bind(this);
+    this.buttonClickHandler = this.buttonClickHandler.bind(this);
+  }
 
-    //call back function
-    buttonClickHandler() {
-		this.setState({renderBall:true});
-	}
-    renderChoice() {
-		if(this.state.renderBall){return <div className="ball" style={this.state.ballPosition}></div>}
-		 else   return <button onClick={this.buttonClickHandler} >Click For One Ball</button>
-    }
-	
-	handleResize = (event) => {
-		if(event.keyCode === 39){
-			let position = this.state.posi;
-			position += 5;
-			this.setState { posi : position, ballPosition : {left : `${position}px`}};
-		}
-	}
+  //call back function
+  buttonClickHandler() {
+    this.setState({ renderBall: true });
+    console.log(this.state.renderBall);
+  }
+  renderChoice() {
+    if (this.state.renderBall) {
+      return <div className="ball" style={this.state.ballPosition}></div>;
+    } else
+      return (
+        <button onClick={this.buttonClickHandler}>Click For One Ball</button>
+      );
+  }
 
-    //bind ArrowRight keydown event
-    componentDidMount() {
-      window.addEventListener('onKeyPress', (event) => this.handleResize(event));
+  handleResize = (event) => {
+    console.log(event.keyCode);
+    if (event.keyCode === 39) {
+      console.log(event.keyCode);
+      this.setState({ posi: this.state.posi + 5 });
+      this.setState({ ballPosition: { left: this.state.posi + "px" } });
+      console.log(this.state);
     }
+  };
 
-    render() {
-        return (
-            <div className="playground">
-                {this.renderChoice()}
-            </div>
-        )
-    }
+  //bind ArrowRight keydown event
+  componentDidMount() {
+    document.addEventListener("keydown", (event) => this.handleResize(event));
+  }
+
+  render() {
+    return <div className="playground">{this.renderChoice()}</div>;
+  }
 }
-
 
 export default App;
